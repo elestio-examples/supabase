a59ac1338b180fa184f2758b1905413839dbd75a
jbenguira <z51biz@gmail.com>
2022-09-25 16:24:01 +0300

    fixes

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -5,14 +5,10 @@ apt install nodejs npm -y
 npm install --prefix ./scripts/JWT/ jsonwebtoken
 
 echo "Generating random keys ..."
-
-JWT_SECRET=$(echo $RANDOM | md5sum | head -c 32; echo;)
 JWT_ANON_KEY=$(JWT_SECRET=$JWT_SECRET node ./scripts/JWT/jwt.js anon);
 JWT_SERVICE_KEY=$(JWT_SECRET=$JWT_SECRET node ./scripts/JWT/jwt.js service_role);
 
 #replace vars in .env and ./volumes/api/kong.yml
-
-sed -i "s/JWT_SECRET_RANDOM/$JWT_SECRET/g" .env
 sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" .env
 sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" .env
 sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" ./volumes/api/kong.yml
