e0bf1fa1646d3bf48e52a74194e58ceee75bc135
jbenguira <z51biz@gmail.com>
2022-09-25 16:34:06 +0300

    fix redeploy

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -8,8 +8,7 @@ echo "Generating random keys ..."
 JWT_ANON_KEY=$(JWT_SECRET=$JWT_SECRET node ./scripts/JWT/jwt.js anon);
 JWT_SERVICE_KEY=$(JWT_SECRET=$JWT_SECRET node ./scripts/JWT/jwt.js service_role);
 
-#replace vars in .env and ./volumes/api/kong.yml
-sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" .env
-sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" .env
+sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" docker-compose.yml
+sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" docker-compose.yml
 sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" ./volumes/api/kong.yml
 sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" ./volumes/api/kong.yml
