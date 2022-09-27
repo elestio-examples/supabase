9d90c3d4f7e3fe402106ac640bfa4ab69f05042f
jbenguira <z51biz@gmail.com>
2022-09-27 21:30:44 +0300

    fixes

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -13,3 +13,9 @@ sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" ./volumes/api/kong.yml
 
 sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" ./keys.env
 sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" ./keys.env
+
+
+echo "SUPABASE_ANON_KEY=$JWT_ANON_KEY" >> ./keys.env
+echo "SUPABASE_SERVICE_KEY=$JWT_SERVICE_KEY" >> ./keys.env
+echo "ANON_KEY=$JWT_ANON_KEY" >> ./keys.env
+echo "SERVICE_KEY=$JWT_SERVICE_KEY" >> ./keys.env
