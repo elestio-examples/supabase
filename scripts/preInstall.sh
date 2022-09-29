7e553e1c72d9ea9bc76f3d6849e0ab4aa3792c35
Joseph Benguira <z51biz@gmail.com>
2022-09-29 18:27:41 +0300

    removed useless sed commands

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -11,9 +11,6 @@ JWT_SERVICE_KEY=$(JWT_SECRET=$JWT_SECRET node ./scripts/JWT/jwt.js service_role)
 sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" ./volumes/api/kong.yml
 sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" ./volumes/api/kong.yml
 
-sed -i "s/ANON_KEY_RANDOM/$JWT_ANON_KEY/g" ./keys.env
-sed -i "s/SERVICE_ROLE_KEY_RANDOM/$JWT_SERVICE_KEY/g" ./keys.env
-
 
 echo "SUPABASE_ANON_KEY=$JWT_ANON_KEY" >> ./keys.env
 echo "SUPABASE_SERVICE_KEY=$JWT_SERVICE_KEY" >> ./keys.env
